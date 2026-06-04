ca267156ea13a2551ebb401c371e54681ac0cdde
maggie_e_matthias <magdolinemad2007@gmail.com>
2026-06-05 01:23:05 +0300

    Added gravity attributes to bird class

--- FlappyBird/Bird.lua
+++ FlappyBird/Bird.lua
@@ -1,5 +1,7 @@
 Bird = Class{}
 
+local Gravity = 20 
+
 function Bird:init()
     --load bird from images on PC
     self.image = love.graphics.newImage('bird.png')
@@ -9,10 +11,17 @@ function Bird:init()
     --Place bird mid-screen
     self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
     self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+    self.dy = 0
 end
 
 function Bird:render()
     love.graphics.draw(self.image, self.x, self.y)
 end
 
+function Bird:update(dt)
+    --Apply gravity to bird's velocity 
+    self.dy = self.dy + Gravity * dt
 
+    --Apply that velocity to the Y position
+    self.y = self.y + self.dy
+end
