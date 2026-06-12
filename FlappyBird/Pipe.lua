07daae30b8efe16c6a1ff32d30c9171ff2fd4e03
maggie_e_matthias <magdolinemad2007@gmail.com>
2026-06-13 00:34:17 +0300

    Fixed a definition issue in Pipe.lua

--- FlappyBird/Pipe.lua
+++ FlappyBird/Pipe.lua
@@ -1,6 +1,5 @@
 Pipe = Class{}
 
-PIPE_IMAGE = gTextures['pipe']
 PIPE_SCROLL = -60 
 
 PIPE_HEIGHT = 430
@@ -13,6 +12,8 @@ function Pipe:init(orientation, y)
     self.width = PIPE_WIDTH
     self.height = PIPE_HEIGHT
     self.orientation = orientation
+
+    self.image = gTextures['pipe']
 end
 
 function Pipe:update(dt)
@@ -20,7 +21,7 @@ function Pipe:update(dt)
 end
 
 function Pipe:render()
-    love.graphics.draw(PIPE_IMAGE, self.x,
+    love.graphics.draw(self.image, self.x,
     (self.orientation == 'top' and self.y + PIPE_HEIGHT or self.y),
     0, 1, self.orientation == 'top' and -1 or 1) 
 end
