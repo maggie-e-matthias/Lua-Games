44f64b4dc9b11c08195e07015f2bd189540d8a4c
maggie_e_matthias <magdolinemad2007@gmail.com>
2026-06-12 22:01:46 +0300

    Completed PlayState

--- FlappyBird/states/PlayState.lua
+++ FlappyBird/states/PlayState.lua
@@ -40,9 +40,64 @@ function PlayState:update(dt)
             if pair.x + PIPE_WIDTH < self.bird.x then 
                 self.score = self.score + 1
                 pair.scored = true
+                sounds['score']:play()
+            end
+        end
+
+        pair:update(dt)
+    end
+
+    -- removes pipe pairs once off-screen
+    -- if done in the loop above, indicies get mixed up
+    for k, pair in pairs(self.pipePairs) do
+        if pair.remove then 
+            table.remove(self.pipePairs, k)
+        end
+    end
+
+    -- collision detection 
+    for k, pair in pairs(self.pipePairs) do
+        for l, pipe in pairs(pair.pipes) do
+            if self.bird:collides(pipe) then
+                sounds['explosion']:play()
+                sounds['hurt']:play()
+
+                gStateMachine:change('score', {
+                    score = self.score
+                })
             end
         end
     end
 
-    pair:update(dt)
+    -- update the bird
+    self.bird:update(dt)
+
+    -- ground collision detection 
+    if self.bird.y > VIRTUAL_HEIGHT - 15 then
+        sounds['explosion']:play()
+        sounds['hurt']:play()
+
+        gStateMachine:change('score', {
+            score = self.score
+        })  
+    end
+end
+
+function PlayState:render()
+    for k, pair in pairs(self.pipePairs) do
+        pair:render()
+    end
+
+    love.graphics.setFont(flappyFont)
+    love.graphics.print('Score: ' .. tostring(self.score), 8, 8)
+
+    self.bird:render()
+end
+
+function PlayState:enter()
+    scrolling = true
+end
+
+function PlayState:exit()
+    scrolling = false
 end
